f52cffbc5e5b8d8587e9b035e4e42ae3d87d49e9
Nathan Glover <nathan@glovers.id.au>
2023-04-08 17:55:06 +1000

    testing if deploy succeeds

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ -n "${INPUT_AWS_ACCESS_KEY_ID}" ] && [ -n "${INPUT_AWS_SECRET_ACCESS_KEY}" 
 fi
 
 export AWS_REGION="${INPUT_AWS_REGION}"
-export INPUT_DEPLOYMENT_ID="${INPUT_DEPLOYMENT_ID}"
+export DEPLOYMENT_ID="${INPUT_DEPLOYMENT_ID}"
 
 chmod +x /greengrass_status.py
 
