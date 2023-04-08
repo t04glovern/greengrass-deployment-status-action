f024df5308104aa6696e8f883c786fa6c0cfeffa
Nathan Glover <nathan@glovers.id.au>
2023-04-08 17:43:21 +1000

    testing if deploy succeeds

--- greengrass_status.py
+++ greengrass_status.py
@@ -1,50 +1,25 @@
 import os
 import time
 import boto3
-from github import Github
 
 def get_greengrass_deployment_status():
-    client = boto3.client('greengrassv2')
+    client = boto3.client('greengrassv2', region_name=os.environ['AWS_REGION'])
     response = client.get_deployment(
-        deploymentId=os.environ['INPUT_DEPLOYMENT_ID']
+        deploymentId=os.environ['DEPLOYMENT_ID']
     )
-
-    return response['deployment']
-
-def update_pr_comment(deployment):
-    g = Github(os.environ['INPUT_GITHUB_TOKEN'])
-    repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
-    pr = repo.get_pull(int(os.environ['INPUT_PULL_REQUEST_NUMBER']))
-    comments = pr.get_issue_comments()
-
-    deployment_status_md = f"""
-**Deployment Name:** {deployment['deploymentName']}
-**Deployment ID:** {deployment['deploymentId']}
-**Time since deploy:** {deployment['createdAt']}
-**Status:** {deployment['status']}
-"""
-
-    comment_id = None
-    for comment in comments:
-        if comment.user.login == os.environ['GITHUB_ACTOR']:
-            comment_id = comment.id
-            break
-
-    if comment_id:
-        comment = repo.get_issue_comment(comment_id)
-        comment.edit(deployment_status_md)
-    else:
-        pr.create_issue_comment(deployment_status_md)
+    return response['deploymentStatus']
 
 def main():
     while True:
-        deployment = get_greengrass_deployment_status()
-        update_pr_comment(deployment)
+        deployment_status = get_greengrass_deployment_status()
+        print(f"Deployment status: {deployment_status}")
+        if deployment_status in ['ACTIVE', 'FAILED']:
+            if deployment_status == 'FAILED':
+                return 1
+            else:
+                return 0
 
-        if deployment['status'] in ['SUCCEEDED', 'FAILED']:
-            break
-
-        time.sleep(60)
+        time.sleep(20)
 
 if __name__ == '__main__':
     main()
